3eb600134f226109ae58439e9e692a33ec617d9f
devops206 <nicholas.king@wayapaychat.com>
2022-12-21 09:24:05 +0100

    fixed resource dependency

--- bonus-point/main.tf
+++ bonus-point/main.tf
@@ -56,11 +56,11 @@ data "aws_availability_zones" "available" {
 }
 data "aws_vpc" "prod_vpc" {}
 
-resource "aws_subnet" "private_subnets" {
-    count = 3
-    vpc_id = data.aws_vpc.prod_vpc.id
-    cidr_block        = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-    availability_zone = slice(data.aws_availability_zones.available.names,0,4)
+data "aws_subnet" "private_subnets" {
+    # count = 3
+    # vpc_id = data.aws_vpc.prod_vpc.id
+    # cidr_block        = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+    # availability_zone = slice(data.aws_availability_zones.available.names,0,4)
 
     tags = {
       "kubernetes.io/cluster/${aws_eks_cluster.jumia.name}" = "shared"
