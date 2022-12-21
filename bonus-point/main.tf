fbc767cc3c97bd3ff4acdb3820b64e1a0addab2d
devops206 <nicholas.king@wayapaychat.com>
2022-12-21 08:45:12 +0100

    fixed resource dependency

--- bonus-point/main.tf
+++ bonus-point/main.tf
@@ -43,43 +43,10 @@ resource "aws_eks_cluster" "jumia" {
   }
 
   depends_on = [
-    aws_subnet.private_subnets,
     aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
   ]
   
-}
-
-################################################
-# create private and public subnets in AZs
-################################################
-data "aws_availability_zones" "available" {
-    state = "available"
-}
-data "aws_vpc" "prod_vpc" {}
-
-resource "aws_subnet" "private_subnets" {
-    count = 3
-    vpc_id = data.aws_vpc.prod_vpc.id
-    cidr_block        = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-    availability_zone = slice(data.aws_availability_zones.available.names,0,4)
-
-    tags = {
-      "kubernetes.io/cluster/${aws_eks_cluster.jumia}" = "shared"
-      "kubernetes.io/role/internal-elb"                 = 1
-    }
-}
-
-resource "aws_subnet" "public_subnets" {
-    count = 3
-    vpc_id = data.aws_vpc.prod_vpc.id
-    cidr_block        = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
-    availability_zone = slice(data.aws_availability_zones.available.names,0,4)
-
-    tags = {
-       "kubernetes.io/cluster/${aws_eks_cluster.jumia}" = "shared"
-       "kubernetes.io/role/elb"                          = 1
-    }
 }
 
 ###############################################################
--- bonus-point/sg.tf
+++ bonus-point/sg.tf
@@ -10,3 +10,35 @@ resource "aws_security_group" "node_group_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+################################################
+# create private and public subnets in AZs
+################################################
+data "aws_availability_zones" "available" {
+    state = "available"
+}
+data "aws_vpc" "prod_vpc" {}
+
+resource "aws_subnet" "private_subnets" {
+    count = 3
+    vpc_id = data.aws_vpc.prod_vpc.id
+    cidr_block        = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+    availability_zone = slice(data.aws_availability_zones.available.names,0,4)
+
+    tags = {
+      "kubernetes.io/cluster/${aws_eks_cluster.jumia}" = "shared"
+      "kubernetes.io/role/internal-elb"                 = 1
+    }
+}
+
+resource "aws_subnet" "public_subnets" {
+    count = 3
+    vpc_id = data.aws_vpc.prod_vpc.id
+    cidr_block        = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+    availability_zone = slice(data.aws_availability_zones.available.names,0,4)
+
+    tags = {
+       "kubernetes.io/cluster/${aws_eks_cluster.jumia}" = "shared"
+       "kubernetes.io/role/elb"                          = 1
+    }
+}
